f8b0c38782a3ea03964111e55aa7867970efe4ff
Bryan Heu <bheuclickbooq@gmail.com>
2013-04-24 15:30:24 -0700

    working User CRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		User.create(params[:user])
+		redirect_to users_path
+	end	
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def destroy
+		User.delete(params[:id])
+
+		redirect_to users_path
+	end
+
+	def update
+		user = User.find(params[:id])
+		user.update_attributes(params[:user])
+
+		redirect_to users_path
+	end
+
+end
